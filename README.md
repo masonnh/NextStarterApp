57201fa1a8997f0c62eb4b152dd56b5368266179
Mason Hunter <masonlizard@gmail.com>
2024-11-13 18:58:25 -0700

    update env vars and README

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # NextJS Starter App
 A starter app in NextJS.
 
+## Setup
+### Environment Variables
+You will need a .env.local file located in the root of your project directory.
+This file should have the following variables (replace TODO with the corresponding value):
+
+General Variables
+- NEXT_PUBLIC_ENVIRONMENT=TODO
+
+Development Environment
+- NEXT_PUBLIC_BASE_URL_DEV=TODO
+- NEXT_PUBLIC_SUPABASE_URL_DEV=TODO
+- NEXT_PUBLIC_SUPABASE_ANON_KEY_DEV=TODO
+
+Production Environment
+- NEXT_PUBLIC_BASE_URL_PROD=TODO
+- NEXT_PUBLIC_SUPABASE_URL_PROD=TODO
+- NEXT_PUBLIC_SUPABASE_ANON_KEY_PROD=TODO
+
+## Tech Stack
+- NextJS
+- Typescript
+- Supabase
+- Google OAuth
 
 ## Pages
 - Home/Landing Page
--- src/lib/env.js
+++ src/lib/env.js
@@ -22,42 +22,5 @@ export const getEnvVars = () => {
             ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_PROD
             : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_DEV,
 
-        // Quickbooks
-        quickbooksEnvironment: isProduction
-            ? process.env.QUICKBOOKS_ENVIRONMENT_PROD
-            : process.env.QUICKBOOKS_ENVIRONMENT_DEV,
-
-        quickbooksClientId: isProduction
-            ? process.env.QUICKBOOKS_CLIENT_ID_PROD
-            : process.env.QUICKBOOKS_CLIENT_ID_DEV,
-
-        quickbooksSecretKey: isProduction
-            ? process.env.QUICKBOOKS_SECRET_KEY_PROD
-            : process.env.QUICKBOOKS_SECRET_KEY_DEV,
-
-        quickbooksAccountingApi: isProduction
-            ? process.env.QUICKBOOKS_ACCOUNTING_API_PROD
-            : process.env.QUICKBOOKS_ACCOUNTING_API_DEV,
-
-        quickbooksPaymentsApi: isProduction
-            ? process.env.QUICKBOOKS_PAYMENTS_API_PROD
-            : process.env.QUICKBOOKS_PAYMENTS_API_DEV,
-
-        // Plaid
-        plaidEnvironment: isProduction
-            ? process.env.PLAID_ENVIRONMENT_PROD
-            : process.env.PLAID_ENVIRONMENT_DEV,
-
-        plaidClientId: isProduction
-            ? process.env.PLAID_CLIENT_ID_PROD
-            : process.env.PLAID_CLIENT_ID_DEV,
-
-        plaidSecretKey: isProduction
-            ? process.env.PLAID_SECRET_KEY_PROD
-            : process.env.PLAID_SECRET_KEY_DEV,
-
-        plaidEndpoint: isProduction
-            ? process.env.PLAID_ENDPOINT_PROD
-            : process.env.PLAID_ENDPOINT_DEV,
     };
 };
